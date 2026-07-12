d37951bfbc4170a3476fbd4d1a69db7b94a2320d
FyukMdaa <fyukmdaa@tutanota.com>
2026-07-12 23:53:34 +0900

    add theme,minibuffa,and more!

--- lock/flake.nix
+++ lock/flake.nix
@@ -7,6 +7,71 @@
       repo = "compat";
       type = "github";
     };
+    consult = {
+      flake = false;
+      owner = "minad";
+      repo = "consult";
+      type = "github";
+    };
+    consult-dir = {
+      flake = false;
+      owner = "karthink";
+      repo = "consult-dir";
+      type = "github";
+    };
+    consult-eglot = {
+      flake = false;
+      owner = "mohkale";
+      repo = "consult-eglot";
+      type = "github";
+    };
+    consult-flycheck = {
+      flake = false;
+      owner = "minad";
+      repo = "consult-flycheck";
+      type = "github";
+    };
+    corfu = {
+      flake = false;
+      owner = "minad";
+      repo = "corfu";
+      type = "github";
+    };
+    corfu-terminal = {
+      flake = false;
+      type = "git";
+      url = "https://codeberg.org/akib/emacs-corfu-terminal";
+    };
+    eglot-booster = {
+      flake = false;
+      owner = "jdtsmith";
+      repo = "eglot-booster";
+      type = "github";
+    };
+    embark = {
+      flake = false;
+      owner = "oantolin";
+      repo = "embark";
+      type = "github";
+    };
+    embark-consult = {
+      flake = false;
+      owner = "oantolin";
+      repo = "embark";
+      type = "github";
+    };
+    flycheck = {
+      flake = false;
+      owner = "flycheck";
+      repo = "flycheck";
+      type = "github";
+    };
+    marginalia = {
+      flake = false;
+      owner = "minad";
+      repo = "marginalia";
+      type = "github";
+    };
     minions = {
       flake = false;
       owner = "tarsius";
@@ -37,6 +102,29 @@
       repo = "nano-modeline";
       type = "github";
     };
+    nerd-icons = {
+      flake = false;
+      owner = "rainstormstudio";
+      repo = "nerd-icons.el";
+      type = "github";
+    };
+    nerd-icons-corfu = {
+      flake = false;
+      owner = "LuigiPiucco";
+      repo = "nerd-icons-corfu";
+      type = "github";
+    };
+    orderless = {
+      flake = false;
+      owner = "oantolin";
+      repo = "orderless";
+      type = "github";
+    };
+    popon = {
+      flake = false;
+      type = "git";
+      url = "https://codeberg.org/akib/emacs-popon";
+    };
     setup = {
       flake = false;
       type = "git";
@@ -46,6 +134,23 @@
       flake = false;
       owner = "emacs-twist";
       repo = "twist.el";
+      type = "github";
+    };
+    undo-fu = {
+      flake = false;
+      type = "git";
+      url = "https://codeberg.org/ideasman42/emacs-undo-fu.git";
+    };
+    vertico = {
+      flake = false;
+      owner = "minad";
+      repo = "vertico";
+      type = "github";
+    };
+    vundo = {
+      flake = false;
+      owner = "casouri";
+      repo = "vundo";
       type = "github";
     };
     which-key = {
